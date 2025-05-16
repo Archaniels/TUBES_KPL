17a48207df18b8bd4685c3584f0e243bac4b3fcf
ADMIRAL MANAN BADAWI <fikricrb001@gmail.com>
2025-05-16 23:41:01 +0700

    coba commit

--- Pembayaran.cs
+++ Pembayaran.cs
@@ -8,14 +8,14 @@ namespace TUBES_KPL
 {
     class Pembayaran
     {
-        private String id;
+        private String idd;
         private String nama;
         private double total;
         private String metodePembayaran;
         private String status;
         private String date;
 
-        public Pembayaran(String id, String nama, String total, String metodePembayaran, String status, String date)
+        public Pembayaran(String idd, String nama, String total, String metodePembayaran, String status, String date)
         {
             
         }
