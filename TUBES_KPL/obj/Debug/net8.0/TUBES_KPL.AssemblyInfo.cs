aac0278f54cd43e196ca92299744f060f43d20db
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-06-05 21:45:08 +0700

    Inisialisasi GUI

--- TUBES_KPL/obj/Debug/net8.0/TUBES_KPL.AssemblyInfo.cs
+++ TUBES_KPL/obj/Debug/net8.0/TUBES_KPL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TUBES_KPL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b6c0b247eea0a5b284dc686c97361196dffd73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ca043ed922a2d54d72416377e5550f47d807f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TUBES_KPL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TUBES_KPL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
